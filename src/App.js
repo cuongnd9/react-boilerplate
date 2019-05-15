fa129efe06b485a592ad7ef7195dab97e3547bc4
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-15 21:32:22 +0700

    Complete setup React-Router (#7)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ReposContainer } from './containers';
+import { Navbar, NotFound } from './components';
 
 function App() {
   return (
-    <div className="container p-5">
-      <p className="text-primary">React Boilerplate</p>
-      <ReposContainer />
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={ReposContainer} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
