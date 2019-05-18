e83ab450967e6481542c1555d77e8e5a0a8ebf51
Cuong Duy Nguyen <ndc07.it@gmail.com>
2019-05-18 14:18:01 +0700

    Complete setup Testing tools (#9)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<App debug />);
+
+    expect(component).toMatchSnapshot();
+  });
 });
