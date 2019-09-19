483021f79944e2ddfbe0909de655789be3be3123
Cuong Duy Nguyen <cuongw.me@gmail.com>
2019-09-19 14:13:41 +0700

    useSelector (#82)

--- src/routes/RepoDetail/index.js
+++ src/routes/RepoDetail/index.js
@@ -1,1 +1,49 @@
-export { default } from './container';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Row, Col, Breadcrumb, Icon, Typography, Spin } from 'antd';
+import { Link } from 'react-router-dom';
+import { fetchAction, clearAction } from 'models/repo/actions';
+import './style.css';
+
+const { Title, Text } = Typography;
+
+function RepoDetail({ match }) {
+  const dispatch = useDispatch();
+
+  const repo = useSelector(state => state.repo);
+
+  useEffect(() => {
+    dispatch(fetchAction(match.params.name));
+    return () => dispatch(clearAction());
+  }, [dispatch, match.params.name]);
+
+  return (
+    <Row>
+      <Col span={20} offset={2} style={{ paddingTop: '50px' }}>
+        <Breadcrumb>
+          <Link to="/">
+            <Breadcrumb.Item href="">
+              <Icon type="home" />
+            </Breadcrumb.Item>
+          </Link>
+          <Breadcrumb.Item>Repo</Breadcrumb.Item>
+        </Breadcrumb>
+        <Title level={4} style={{ marginTop: '20px' }}>
+          {repo.name}
+        </Title>
+        {Object.keys(repo).length === 0 && <Spin />}
+        {Object.keys(repo).length > 0 && (
+          <>
+            <p className="text-primary">{repo.full_name}</p>
+            <p className="text-secondary">{repo.description}</p>
+            <Text mark className="text-warning">
+              {repo.language}
+            </Text>
+          </>
+        )}
+      </Col>
+    </Row>
+  );
+}
+
+export default RepoDetail;
--- src/routes/Repos/index.js
+++ src/routes/Repos/index.js
@@ -1,1 +1,42 @@
-export { default } from './container';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Row, Col, Typography, Card, Spin } from 'antd';
+import { Link } from 'react-router-dom';
+import { fetchAction } from 'models/repos/actions';
+import './style.css';
+
+const { Title } = Typography;
+
+function Repos() {
+  const dispatch = useDispatch();
+  const repos = useSelector(state => state.repos);
+
+  useEffect(() => {
+    dispatch(fetchAction());
+  }, [dispatch]);
+
+  return (
+    <Row>
+      <Col span={20} offset={2} style={{ paddingTop: '50px' }}>
+        <Title level={4}>My Repos</Title>
+        {repos.length === 0 && <Spin />}
+        <Row gutter={16}>
+          {repos.length !== 0 &&
+            repos.map(repo => (
+              <Col key={repo.id} span={12}>
+                <Card
+                  title={repo.name}
+                  extra={<Link to={`/${repo.name}`}>More</Link>}
+                  style={{ marginBottom: '15px' }}
+                >
+                  <p>{repo.description}</p>
+                </Card>
+              </Col>
+            ))}
+        </Row>
+      </Col>
+    </Row>
+  );
+}
+
+export default Repos;
