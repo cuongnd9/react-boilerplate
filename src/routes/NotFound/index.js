bff7f80d1e3142f942ee17b28b8b0d422dab2839
Nguyen Duy Cuong <cuongw.me@gmail.com>
2019-09-19 14:25:15 +0700

    Add link for login route

--- src/routes/NotFound/index.js
+++ src/routes/NotFound/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar } from 'components';
 import './style.css';
 
 function NotFound() {
   return (
-    <div className="notfound">
-      <div className="notfound-404">
-        <h1>404</h1>
+    <>
+      <Navbar />
+      <div className="notfound">
+        <div className="notfound-404">
+          <h1>404</h1>
+        </div>
+        <h2>Oops! Nothing was found</h2>
+        <p>
+          The page you are looking for might have been removed had its name changed or is
+          temporarily unavailable. <Link to="/">Return to homepage</Link>
+        </p>
       </div>
-      <h2>Oops! Nothing was found</h2>
-      <p>
-        The page you are looking for might have been removed had its name changed or is temporarily
-        unavailable. <Link to="/">Return to homepage</Link>
-      </p>
-    </div>
+    </>
   );
 }
 
