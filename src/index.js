1edfec916ca37677ab706fe03fa4a5bf11ddf15a
Cuong Duy Nguyen <cuongw.me@gmail.com>
2020-02-07 22:04:12 +0700

    Move store.js file to src folder (#94)

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './helpers/store';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
